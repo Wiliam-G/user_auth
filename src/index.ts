5f636d7ccab8fd9dfa4eb61b8466310f434f6bc6
wiliam G <wiliam.gomes99@gmail.com>
2022-05-30 06:39:58 -0300

    Configurando o servidor e adicionando scripts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('teste');
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+// const port = 3000; 
+
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send ({teste: "ok sucesso mano"});
+});
+
+app.listen(3000, () => {
+    console.log('rodando na porta 3000');
+})
